e737f1af8236fa7b4a83d51dfbaf546768195e43
FerShibli <fernandoluiz.1991@gmail.com>
2024-09-27 20:17:47 -0300

    Adicionar inicialização aleatória de matriz

--- arrays_matrizes_collections/arrays_matrizes_collections/src/main/java/com/fershibli/ed/aula01/arrays_matrizes_collections/Arrays_matrizes_collections.java
+++ arrays_matrizes_collections/arrays_matrizes_collections/src/main/java/com/fershibli/ed/aula01/arrays_matrizes_collections/Arrays_matrizes_collections.java
@@ -22,6 +22,25 @@ public class Arrays_matrizes_collections {
         mat = new int[lin][col];
         diag = new int[lin];
         
-        System.out.println("Hello World!");
+        String resposta = "Colunas";
+        col = 0;
+        for (int[] m  : mat) {
+            for (int coluna = 0; coluna < m.length; coluna++) {
+                m[coluna] = (int) (Math.random() * 10);
+            }
+            resposta += "\t" + col++;
+        }
+        
+        resposta += "\nLinhas";
+        lin = 0;
+        for (int[] m  : mat) {
+            resposta += "\n" + lin++;
+            for (int coluna = 0; coluna < m.length; coluna++) {
+                m[coluna] = (int) (Math.random() * 10);
+                resposta += "\t" + m[coluna];
+            }
+        }
+        
+        System.out.println(resposta);
     }
 }
