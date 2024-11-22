0eaa2750d0843c456850aadb5942d21b0978ad3c
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2024-11-22 20:17:07 -0300

    Adicionar estrutura inicial do projeto DicionarioExercicio4 com classe Dicionario e arquivos de configuração

---
+++ DicionarioExercicio4/src/dicionarioexercicio4/Dicionario.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dicionarioexercicio4;
+
+/**
+ *
+ * @author Alunos
+ */
+import java.util.HashMap;
+ 
+class Dicionario {
+ 
+    private HashMap<String, String> palavras;
+ 
+    public Dicionario() {
+        this.palavras = new HashMap<>();
+    }
+ 
+    public void addPalavra(String palavra, String definicao) {
+        palavras.put(palavra, definicao);
+    }
+ 
+    public String getDefinicao(String palavra) {
+        return palavras.getOrDefault(palavra, "Definição não encontrada.");
+    }
+ 
+    public HashMap<String, String> getTodasPalavras() {
+        return palavras;
+    }
+    
+    /*
+    Exercício Complementar: Dicionário com Funcionalidades Avançadas
+    Além das funcionalidades básicas, adicione as seguintes funcionalidades ao seu dicionário:
+
+    Remover Palavra:
+        Implemente um método removePalavra(String word) que remove uma palavra e sua definição do dicionário.
+    Atualizar Definição:
+        Implemente um método atualizaDefinicao(String word, String newDefinition) que atualiza a definição de uma palavra existente.
+    Contar Palavras:
+        Implemente um método contaPalavras() que retorna o número total de palavras no dicionário.
+    */
+}
