0c5978adf7336af444ff81b4b2d44e705f77f523
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2024-11-22 21:05:33 -0300

    Adicionar exemplo de uso do dicionário com operações de adição, atualização, remoção e listagem de palavras

--- DicionarioExercicio4/src/dicionarioexercicio4/DicionarioExercicio4.java
+++ DicionarioExercicio4/src/dicionarioexercicio4/DicionarioExercicio4.java
@@ -14,7 +14,44 @@ public class DicionarioExercicio4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Dicionario dicionario = new Dicionario();
+ 
+        // Adicionando algumas palavras e definições
+        dicionario.addPalavra("maçã", "Uma fruta.");
+        dicionario.addPalavra("cachorro", "Um animal domesticado pelo homem.");
+        dicionario.addPalavra("computador", "Um dispositivo eletrônico para processamento de dados.");
+ 
+        // Recuperando a definição de uma palavra
+        String definicao = dicionario.getDefinicao("cachorro");
+        System.out.println("Definição de 'cachorro': " + definicao);
+ 
+        // Listando todas as palavras no dicionário
+        System.out.println("Palavras no dicionário:");
+        for (String palavra : dicionario.getTodasPalavras().keySet()) {
+            System.out.println("- " + palavra);
+        }
+ 
+        System.out.println("Significados das Palavras no dicionário:");
+        for (String palavra : dicionario.getTodasPalavras().values()) {
+            System.out.println("- " + palavra);
+        }
+ 
+        System.out.println("Palavras e significados no dicionário:");
+        System.out.println(dicionario.getTodasPalavras());
+        
+        
+        System.out.println("Quantidade de palavras no dicionário: "+dicionario.contaPalavras());
+        dicionario.atualizaDefinicao("cachorro", "Um animal descendente do Lobo");
+        System.out.println("Definição de cachorro:"+dicionario.getDefinicao("cachorro"));
+        
+        dicionario.atualizaDefinicao("banana", "uma fruta rica em potássio");
+        
+        System.out.println("Quantidade de palavras no dicionário: "+dicionario.contaPalavras());
+        dicionario.removePalavra("maçã");
+        System.out.println("Quantidade de palavras no dicionário: "+dicionario.contaPalavras());
+        System.out.println("Definição de maçã: "+dicionario.getDefinicao("maçã"));
+        System.out.println("Quantidade de palavras no dicionário: "+dicionario.contaPalavras());
+        
     }
     
 }
