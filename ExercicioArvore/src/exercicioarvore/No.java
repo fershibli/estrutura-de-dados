5f9540e35e10a1151fd6772957dc399ca95fb600
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2024-11-29 22:04:40 -0300

    Adicionar classe No com métodos para inserção em árvore binária

--- ExercicioArvore/src/exercicioarvore/No.java
+++ ExercicioArvore/src/exercicioarvore/No.java
@@ -9,5 +9,28 @@ package exercicioarvore;
  * @author Alunos
  */
 public class No {
+    No esquerdo;
+    int valor;
+    No direito;
     
+    public No(int valor) {
+        this.valor = valor;
+        esquerdo = direito = null;
+    }
+    
+    public void inserir(int valor) {
+        if (valor<this.valor) {
+            if (esquerdo == null) {
+                esquerdo = new No(valor);
+            } else {
+                esquerdo.inserir(valor);
+            }
+        } else if(valor > this.valor) {
+            if (direito == null) {
+                direito = new No(valor);
+            } else {
+                direito.inserir(valor);
+            }
+        }
+    }
 }
