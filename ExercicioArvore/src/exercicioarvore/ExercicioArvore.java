fae703c965fe594998ec5af1d345130281b7a0b9
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2024-11-29 22:04:58 -0300

    Adicionar lógica de inserção e percursos na classe ExercicioArvore

--- ExercicioArvore/src/exercicioarvore/ExercicioArvore.java
+++ ExercicioArvore/src/exercicioarvore/ExercicioArvore.java
@@ -14,7 +14,25 @@ public class ExercicioArvore {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Arvore arvore = new Arvore();
+
+        int valores[] = {120, 44, 4, 63, 48, 15, 85, 1, 99, 18, 20, 7, 70, 20, 5, 9, 86};
+
+        System.out.println("\n\nValores inseridos: ");
+        for (int i = 0;  i < valores.length; i++) {
+            int valor = valores[i];
+            System.out.print(valor + " ");
+            arvore.inserirNo(valor);
+        }
+        System.out.println("\n\n\nPercurso na pré-ordem");
+        arvore.preordemPercurso(); 
+        System.out.println("\n\nPercurso na ordem");
+        arvore.emordemPercurso();
+        System.out.println("\n\nPercurso na pós-ordem");
+        arvore.posordemPercurso();
+        
+
     }
     
 }
